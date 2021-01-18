db1c24663243815ee0ce24d91044ae232dabc2ba
ParcelSolutions <info@parcelsolutions.eu>
2021-01-18 14:05:37 +0100

    fix api schema issue

--- openapi/setup/schemaSetup.js
+++ openapi/setup/schemaSetup.js
@@ -4,13 +4,17 @@ const $RefParser = require("@apidevtools/json-schema-ref-parser");
 
 const shipmentSchema = require("../../transmateSchemas/restAPI/requests/shipmentRequest.json");
 const partnerSchema = require("../../transmateSchemas/restAPI/requests/accountRequest.json");
+
 const schemas = { shipmentSchema, partnerSchema };
+
+
 
 async function writeRequestSchema(name) {
     
   console.log("build schema for ", name);
+  let parser = new $RefParser();
+  let schema = await parser.bundle(schemas[name]);
 
-  let schema = await $RefParser.bundle(schemas[name]);
   const convertedSchema = await convert(schema,{dereference :true});
   fs.writeFileSync(
     "schemas/" + name + ".json",
