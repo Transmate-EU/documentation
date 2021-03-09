d150eb6129bc2788097ce76f405f123556108a02
Jan Carpentier <jan@transamte.eu>
2021-03-09 08:14:21 +0100

    add simulate

--- openapi/setup/schemaSetup.js
+++ openapi/setup/schemaSetup.js
@@ -4,22 +4,23 @@ const $RefParser = require("@apidevtools/json-schema-ref-parser");
 
 const shipmentSchema = require("../../transmateSchemas/restAPI/requests/shipmentRequest.json");
 const partnerSchema = require("../../transmateSchemas/restAPI/requests/accountRequest.json");
-
+const simulationSchema = require("../../transmateSchemas/restAPI/simulate/simulate.json");
 const schemas = { shipmentSchema, partnerSchema };
 
 
 
 async function writeRequestSchema(name) {
-    
+
   console.log("build schema for ", name);
   let parser = new $RefParser();
   let schema = await parser.bundle(schemas[name]);
 
-  const convertedSchema = await convert(schema,{dereference :true});
+  const convertedSchema = await convert(schema, { dereference: true });
   fs.writeFileSync(
     "schemas/" + name + ".json",
     JSON.stringify(convertedSchema)
   );
 }
-writeRequestSchema("partnerSchema");
-writeRequestSchema("shipmentSchema");
+Object.keys(schemas).forEach(key => writeRequestSchema(key))
+// writeRequestSchema("partnerSchema");
+// writeRequestSchema("shipmentSchema");
